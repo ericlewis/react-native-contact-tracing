c9a623016c41f06ca54ff8eb9581b6908d88d6f7
Eric Lewis <ericlewis777@gmail.com>
2020-04-13 17:04:54 -0400

    chore: rename android package

--- android/src/main/java/com/contacttracing/ContactTracingModule.java
+++ android/src/main/java/com/contacttracing/ContactTracingModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.contacttracing;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/contacttracing/ContactTracingPackage.java
+++ android/src/main/java/com/contacttracing/ContactTracingPackage.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.contacttracing;
 
 import java.util.Arrays;
 import java.util.Collections;
