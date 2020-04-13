782821d917c76b676d97f418ab093284b9d8dfb4
Eric Lewis <ericlewis777@gmail.com>
2020-04-13 02:01:39 -0400

    chore: formatting

--- ios/ContactTracing.m
+++ ios/ContactTracing.m
@@ -3,13 +3,13 @@
 
 @interface RCT_EXTERN_MODULE(ContactTracing, RCTEventEmitter)
 
-RCT_EXTERN_METHOD(start: (RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(start:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(stop: (RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(stop:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 
-RCT_EXTERN_METHOD(requestExposureSummary: (RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(requestExposureSummary:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 
 @end
