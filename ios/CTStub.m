73f816110db8ddd621524fc993a595a580b35867
Eric Lewis <ericlewis777@gmail.com>
2020-04-13 19:30:03 -0400

    feat: stub methods

--- ios/CTStub.m
+++ ios/CTStub.m
@@ -10,16 +10,72 @@
 
 @implementation CTExposureDetectionSession
 
+- (void)activateWithCompletion:(nullable CTErrorHandler)inCompletion
+{
+    inCompletion(NULL);
+}
+
+- (void)addPositiveDiagnosisKeys:(NSArray <CTDailyTracingKey *> *)inKeys
+                      completion:(nullable CTErrorHandler)inCompletion
+{
+    inCompletion(NULL);
+}
+
+- (void)finishedPositiveDiagnosisKeysWithCompletion:(nullable CTExposureDetectionFinishHandler)inFinishHandler
+{
+    inFinishHandler(NULL, NULL);
+}
+
+- (void)getContactInfoWithHandler:(nullable CTExposureDetectionContactHandler)inHandler
+{
+    inHandler(NULL, NULL);
+}
+
+- (void)invalidate
+{
+    // NOOP
+}
+
 @end
 
 @implementation CTSelfTracingInfoRequest
+
+- (void)perform
+{
+    // NOOP
+}
+
+- (void)invalidate
+{
+    // NOOP
+}
 
 @end
 
 @implementation CTStateGetRequest
 
+- (void)perform
+{
+    // NOOP
+}
+
+- (void)invalidate
+{
+    // NOOP
+}
+
 @end
 
 @implementation CTStateSetRequest
+
+- (void)perform
+{
+    // NOOP
+}
+
+- (void)invalidate
+{
+    // NOOP
+}
 
 @end
