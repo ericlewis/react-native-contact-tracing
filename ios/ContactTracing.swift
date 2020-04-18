b4e1f70df18d6086221737b3ca2eb931cc2d0d0a
Eloy Durán <eloy.de.enige@gmail.com>
2020-04-18 17:34:28 +0200

    fix: build after updating ContactTracingManager

--- ios/ContactTracing.swift
+++ ios/ContactTracing.swift
@@ -170,25 +170,3 @@ public class ContactTracing: RCTEventEmitter {
         resolve(state.rawValue)
     }
 }
-
-extension CTExposureDetectionSession {
-    func addPositiveDiagnosisKeys(batching keys: [CTDailyTracingKey], completion: CTErrorHandler) {
-        if keys.isEmpty {
-            completion(nil)
-        } else {
-            let cursor = keys.index(keys.startIndex, offsetBy: maxKeyCount, limitedBy: keys.endIndex) ?? keys.endIndex
-            let batch = Array(keys.prefix(upTo: cursor))
-            let remaining = Array(keys.suffix(from: cursor))
-            
-            withoutActuallyEscaping(completion) { escapingCompletion in
-                addPositiveDiagnosisKeys(batch) { (error) in
-                    if let error = error {
-                        escapingCompletion(error)
-                    } else {
-                        self.addPositiveDiagnosisKeys(batching: remaining, completion: escapingCompletion)
-                    }
-                }
-            }
-        }
-    }
-}
